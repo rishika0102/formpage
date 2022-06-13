88ae8db8da21d71763fb1f4dce7bd6d4da76796a
rishika0102 <rishikaa@shriffle.com>
2022-06-13 19:37:49 +0530

    form validation task

--- js/script.js
+++ js/script.js
@@ -1,48 +1,82 @@
-function storedetails() {
+// function checkfields(x) {
+//  var firstname =  document.forms["userdetails"]["firstname"].value;
+//  var lastname = document.forms["userdetails"]["lastname"].value;
+//  var email = document.forms["userdetails"]["email"].value;
+//  var pwd = document.forms["userdetails"]["pwd"].value;
+//  var conpwd = document.forms["userdetails"]["conpwd"].value;
+//  var gend = document.forms["userdetails"]["gend"].value;
+//  var dob = document.forms["userdetails"]["dob"].value;
+//   if (firstname == "" || lastname == "" || email == "" || pwd == "" || conpwd == null || gend == null || dob == null) {
+//    x.style.border = "red solid 5px";
+//    console.log("hello", firstname);
+//     }
+// }
+// function chkfields(z) {
+// 	 let firstname, lastname, email, pwd, conpwd, gend, dob;
+//   firstname =  document.forms["userdetails"]["firstname"].value;
+//   lastname = document.forms["userdetails"]["lastname"].value;
+//   email = document.forms["userdetails"]["email"].value;
+//   pwd = document.forms["userdetails"]["pwd"].value;
+//   conpwd = document.forms["userdetails"]["conpwd"].value;
+//   gend = document.forms["userdetails"]["gend"].value;
+//   dob = document.forms["userdetails"]["dob"].value;
+//   if (firstname != null && lastname != null && email != null && pwd != null && conpwd != null && gend != null && dob != null) {
+//    z.style.border = "green solid 5px";
+//    console.log("world");
+//     }
+// }
+function signUp() {
 
-  let name, email, pwd;
-name = document.getElementById("name").value;
-email = document.getElementById("email").value;
-pwd = document.getElementById("pwd").value;
-
-
-let userrecord =new Array();
-userrecord = JSON.parse(localStorage.getItem("userdetails"))? JSON.parse(localStorage.getItem("userdetails")):[]
-userrecord.push({
-  "name": name,
-  "email": email,
-  "pwd": pwd,
-})
-localStorage.setItem("userdetails", JSON.stringify(userrecord));
-console.log("users.." ,userdetails);
-
-}
-
-function checkvalidation(userrecord) {
-  console.log("checkvalidation");
-  let checkemail, checkpwd;
-  checkemail = document.getElementById("checkemail").value;
-  checkpwd = document.getElementById("pwd").value;
-
-  let checkuserreccord=new Array();
-  checkuserreccord = JSON.parse(sessionStorage.getItem("logindetails"))? JSON.parse(sessionStorage.getItem("logindetails")):[]
-  checkuserreccord.push({
-    "checkemail" : checkemail,
-    "checkpwd" : checkpwd
+  let firstname, lastname, email, pwd, conpwd, gend, dob;
+  firstname =  document.forms["userdetails"]["firstname"].value;
+  lastname = document.forms["userdetails"]["lastname"].value;
+  email = document.forms["userdetails"]["email"].value;
+  pwd = document.forms["userdetails"]["pwd"].value;
+  conpwd = document.forms["userdetails"]["conpwd"].value;
+  gend = document.forms["userdetails"]["gend"].value;
+  dob = document.forms["userdetails"]["dob"].value;
+  if(pwd != conpwd ){
+   alert("password and confrim-password must be same");
+  }
+  let userrecord = new Array();
+  userrecord = JSON.parse(localStorage.getItem("userdetails")) ? JSON.parse(localStorage.getItem("userdetails")) : []
+  userrecord.push({
+    "firstname": firstname,
+    "lastname": lastname,
+    "email": email,
+    "pwd": pwd,
+    "conpwd": conpwd,
+    "gend": gend,
+    "dob": dob
   })
-  sessionStorage.setItem("logindetails", JSON.stringify(checkuserrecord));
-   console.log("logindetails.." ,logindetails);
-   for(let x in userrecord){
-    console.log("for loop");
-  if(checkemail === userrecord[x].email && checkpwd === userrecord[x].pwd){
-  console.log("if.....");
-  // alert("successfully logged in");
+  localStorage.setItem("userdetails", JSON.stringify(userrecord));
+  console.log("userdetails....", userrecord);
+
 }
-else {
-  // alert("invalid credentials");
-  console.log("else.....");
-}
-}
+
+function login() {
+
+  let checkemail, checkpwd;
+  checkemail = document.forms["userdetails"]["checkemail"].value;
+  checkpwd = document.forms["userdetails"]["checkpwd"].value;
+  let checkuserreccord = new Array();
+  checkuserreccord = JSON.parse(localStorage.getItem("userdetails")) ? JSON.parse(localStorage.getItem("userdetails")) : []
+  checkuserreccord.push({
+      "checkemail": checkemail,
+      "checkpwd": checkpwd
+  })
+  var x = JSON.parse(localStorage.getItem("userdetails"));
+  console.log("userdetails....", x);
+  for (let y in userdetails) {
+  	debugger
+  	console.log("login..for",x[y]);
+    if (checkemail == x[y].email && checkpwd == x[y].pwd) {
+    	console.log("match");
+      alert("successfully logged in");
+    } else {
+      alert("invalid credentials");
+      }
+  }
 }
 
 

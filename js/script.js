04e584aeaeec0e0adf25c7eb981c84c3fb5512e4
rishika0102 <rishikaa@shriffle.com>
2022-06-15 23:39:52 +0530

    all the required changes completed

--- js/script.js
+++ js/script.js
@@ -1,87 +1,57 @@
-
-function checkfields(inputelement) {
-
-  const firstName = document.forms["users"]["firstname"].value;
-  const lastName = document.forms["users"]["lastname"].value;
-  const email = document.forms["users"]["email"].value;
-  const pwd = document.forms["users"]["pwd"].value;
-  const conpwd = document.forms["users"]["conpwd"].value;
-  const gend = document.forms["users"]["gend"].value;
-  const dob = document.forms["users"]["dob"].value;
-  if (firstName == "" || lastName == "" || email == "" || pwd == "" || conpwd == null || gend == null || dob == null) {
-  inputelement.style.border = "red solid 5px";
-  console.log("hello");
-  }
-}
-
-function chkfields(inputelement) {
-
-  const firstName = document.forms["users"]["firstname"].value;
-  const lastName = document.forms["users"]["lastname"].value;
-  const email = document.forms["users"]["email"].value;
-  const pwd = document.forms["users"]["pwd"].value;
-  const conpwd = document.forms["users"]["conpwd"].value;
-  const gend = document.forms["users"]["gend"].value;
-  const dob = document.forms["users"]["dob"].value;
-  if (firstName != null && lastName != null && email != null && pwd != null && conpwd != null && gend != null && dob != null) {
-  inputelement.style.border = "green solid 5px";
-  console.log("world");
-  }
+function checkFields(inputElement) {
+ console.log(inputElement);
+ if (inputElement.value === "") {
+  inputElement.style.border = "red solid 2px";
+ }
 }
 
 function signUp() {
+ const firstName = document.forms["users"]["firstname"].value;
+ const lastName = document.forms["users"]["lastname"].value;
+ const email = document.forms["users"]["email"].value;
+ const pwd = document.forms["users"]["pwd"].value;
+ const conPwd = document.forms["users"]["conpwd"].value;
+ const gend = document.forms["users"]["gend"].value;
+ const dob = document.forms["users"]["dob"].value;
 
-  const firstName = document.forms["users"]["firstname"].value;
-  const lastName = document.forms["users"]["lastname"].value;
-  const email = document.forms["users"]["email"].value;
-  const pwd = document.forms["users"]["pwd"].value;
-  const conPwd = document.forms["users"]["conpwd"].value;
-  const gend = document.forms["users"]["gend"].value;
-  const dob = document.forms["users"]["dob"].value;
-  if(document.forms["users"]["firstname"].value == "" ){
+ if (
+  document.forms["users"]["firstname"].value == "" ||
+  document.forms["users"]["lastname"].value == "" ||
+  document.forms["users"]["email"].value == "" ||
+  document.forms["users"]["pwd"].value == "" ||
+  document.forms["users"]["conpwd"].value == ""
+ ) {
+  return false;
+ } else if (document.forms["users"]["conpwd"].value != document.forms["users"]["pwd"].value) {
+    alert("Password and Confrim Password must be similar");
     return false;
-  }
-  else if(document.forms["users"]["lastname"].value == "" ){
-    return false;
-  }
-   else if(document.forms["users"]["email"].value == "" ){
-    return false;
-  }
-   else if(document.forms["users"]["pwd"].value == "" ){
-    return false;
-  }
-   else if(document.forms["users"]["conpwd"].value == "" ){
-    return false;
-  }
-  else{
-  const userrecord = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [];
-  userrecord.push({
-  firstName: firstName,
-  lastName: lastName,
-  email: email,
-  pwd: pwd,
-  conPwd: conPwd,
-  gend: gend,
-  dob: dob,
+ } else {
+    const userrecord = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [];
+    userrecord.push({
+     firstName: firstName,
+     lastName: lastName,
+     email: email,
+     pwd: pwd,
+     conPwd: conPwd,
+     gend: gend,
+     dob: dob,
   });
-  localStorage.setItem("users", JSON.stringify(userrecord));
-  console.log("users....", userrecord);
-}
+    localStorage.setItem("users", JSON.stringify(userrecord));
+    console.log("users....", userrecord);
+ }
 }
 
 function logIn() {
-
-  const checkEmail = document.forms["users"]["checkemail"].value;
-  const checkPwd = document.forms["users"]["checkpwd"].value;
-  const checkuserReccord = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [];
-  checkuserReccord.push({
+ const checkEmail = document.forms["users"]["checkemail"].value;
+ const checkPwd = document.forms["users"]["checkpwd"].value;
+ const checkuserReccord = JSON.parse(localStorage.getItem("users")) ? JSON.parse(localStorage.getItem("users")) : [];
+ checkuserReccord.push({
   checkEmail: checkEmail,
   checkPwd: checkPwd,
-  });
-  var x = JSON.parse(localStorage.getItem("users"));
-  console.log("users....", x);
-  for (let user in users) {
-  debugger;
+ });
+ var x = JSON.parse(localStorage.getItem("users"));
+ console.log("users....", x);
+ for (let user in users) {
   console.log("login..for", user);
   if (checkEmail == x[user].email && checkPwd == x[user].pwd) {
    console.log("match");
@@ -89,7 +59,5 @@ function logIn() {
   } else {
    alert("invalid credentials");
   }
-  }
-
+ }
 }
-
